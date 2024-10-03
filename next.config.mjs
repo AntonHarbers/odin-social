c2cab943cf5750854f8e3f4c4224c0cd21dd8985
Cosmicode <anton.harbers23@gmail.com>
2024-10-03 21:33:57 +0200

    FIX: changes image patterns in next config and updated nextjs version

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com',
-      'utfs.io',
-      'avatar-management--avatars.us-west-2.prod.public.atl-paas.net',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname:
+          'avatar-management--avatars.us-west-2.prod.public.atl-paas.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
